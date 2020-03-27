44ed52f4a74b26ca2ce32b762caa134f3ac63694
Mohammad Khodaei <mohammad19khodaei@gmail.com>
2020-03-27 21:46:15 +0430

    WIP add basic methods (session 4)

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return "hello flask"
+
+
+@app.route('v1/getsms')
+def get_sms():
+    pass
+
+
+def send_sms():
+    pass
+
+
+def check_sms():
+    pass
