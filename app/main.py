a2e416aca71e151ea6bc3daf610dbfc3a6b3d316
Mohammad Khodaei <mohammad19khodaei@gmail.com>
2020-05-25 16:33:29 +0430

    add upload file page

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
-from flask import Flask, request, jsonify, Response, redirect, url_for, abort
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
-from pandas import read_excel
-import requests
-import config
 import sqlite3
 import re
+import os
+from flask import (
+    Flask,
+    request,
+    jsonify,
+    Response,
+    redirect,
+    url_for,
+    abort,
+    flash,
+    get_flashed_messages,
+)
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from pandas import read_excel
+from werkzeug.utils import secure_filename
+import requests
+import config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config["SECRET_KEY"] = config.SECRET_KEY
+app.config["UPLOAD_FOLDER"] = config.UPLOAD_DIRECTORY
 
 # flask login
 login_manager = LoginManager()
@@ -17,48 +30,89 @@ login_manager.login_view = "login"
 
 
 class User(UserMixin):  # silly user model
-
     def __init__(self, id):
         self.id = id
 
 
-@app.route('/')
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in config.ALLOWED_EXTENSIONS
+    )
+
+
+@app.route("/", methods=["GET", "POST"])
 @login_required
 def home():
-    return Response("Hello World!")
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if not allowed_file(file.filename):
+            flash("Not allowd file")
+            return redirect(request.url)
+        else:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(filepath)
+            import_excel_to_db(filepath)
+            flash("excel file uploaded successfully")
+            return redirect(url_for("home"))
+
+    messages = get_flashed_messages()
+    message = messages[0] if messages else ""
+    return f"""
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <h3>{message}</h3>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    """
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         if username == config.USERNAME and password == config.PASSWORD:
             user = User(1)
             login_user(user)
-            return redirect('/')
+            return redirect("/")
         else:
             return abort(401)
     else:
-        return Response('''
+        return Response(
+            """
         <form action="" method="post">
             <p><input type=text name=username>
             <p><input type=password name=password>
             <p><input type=submit value=Login>
         </form>
-        ''')
+        """
+        )
 
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
 
 @app.errorhandler(401)
 def page_not_found(error):
-    return Response('<p>Login failed</p>')
+    return Response("<p>Login failed</p>")
 
 
 @login_manager.user_loader
@@ -66,12 +120,12 @@ def load_user(userid):
     return User(userid)
 
 
-@app.route('/v1/ok')
+@app.route("/v1/ok")
 def health_check():
-    return jsonify({'message': 'ok'}), 200
+    return jsonify({"message": "ok"}), 200
 
 
-@app.route('/v1/process', methods=['POST'])
+@app.route("/v1/process", methods=["POST"])
 def process():
     """ call this method when we receive sms from customers
 
@@ -79,13 +133,13 @@ def process():
         [json] -- [confirmation]
     """
     data = request.form
-    sender = data.get('from')
-    message = normalize_string(data.get('message'))
+    sender = data.get("from")
+    message = normalize_string(data.get("message"))
 
     response = check_serial(message)
 
     send_sms(sender, response)
-    return jsonify({'message': 'your sms message processed'}), 200
+    return jsonify({"message": "your sms message processed"}), 200
 
 
 def send_sms(receiver, message):
@@ -95,11 +149,7 @@ def send_sms(receiver, message):
         sender {[string]} -- [receiver of sms]
         message {[string]} -- [text of sms]
     """
-    data = {
-        'to': receiver,
-        'message': message,
-        'token': config.API_KEY
-    }
+    data = {"to": receiver, "message": message, "token": config.API_KEY}
 
     response = requests.post(config.SMS_SERVER, data=data)
     print(response.text)
@@ -115,9 +165,9 @@ def normalize_string(data, fixed_size=30):
     Returns:
         [string] -- [normalized string]
     """
-    persian_numerals = '۱۲۳۴۵۶۷۸۹۰'
-    arabic_numerals = '١٢٣٤٥٦٧٨٩٠'
-    english_numerals = '1234567890'
+    persian_numerals = "۱۲۳۴۵۶۷۸۹۰"
+    arabic_numerals = "١٢٣٤٥٦٧٨٩٠"
+    english_numerals = "1234567890"
     for index in range(len(persian_numerals)):
         # replace english digits with persian and arabic digits
         data = data.replace(persian_numerals[index], english_numerals[index])
@@ -125,11 +175,11 @@ def normalize_string(data, fixed_size=30):
 
     data = data.upper()
     # remove any none alphanumeric character
-    data = re.sub(r'\W+', '', data)
+    data = re.sub(r"\W+", "", data)
 
     # find all alpha and digits chars
-    all_alpha = ''
-    all_digit = ''
+    all_alpha = ""
+    all_digit = ""
     for char in data:
         if char.isalpha():
             all_alpha += char
@@ -138,7 +188,7 @@ def normalize_string(data, fixed_size=30):
 
     # add enough zero between alpha and digit characters
     number_of_zero = fixed_size - len(all_alpha) - len(all_digit)
-    return all_alpha + ('0' * number_of_zero) + all_digit
+    return all_alpha + ("0" * number_of_zero) + all_digit
 
 
 def import_excel_to_db(file_path):
@@ -159,15 +209,15 @@ def import_excel_to_db(file_path):
     for i, (row, ref, desc, start_serial, end_serial, date) in data_frame.iterrows():
         start_serial = normalize_string(start_serial)
         end_serial = normalize_string(end_serial)
-        query = f'''INSERT INTO serials("reference", "description", "start_serial", "end_serial", "date")
-        VALUES("{ref}", "{desc}", "{start_serial}", "{end_serial}", "{date}")'''
+        query = f"""INSERT INTO serials("reference", "description", "start_serial", "end_serial", "date")
+        VALUES("{ref}", "{desc}", "{start_serial}", "{end_serial}", "{date}")"""
         cursor.execute(query)
     # commit valid serials to serials table
     connection.commit()
 
     # sheet 1 contains failed codes
     data_frame = read_excel(file_path, sheet_name=1)
-    for i, (failed_serial, ) in data_frame.iterrows():
+    for i, (failed_serial,) in data_frame.iterrows():
         failed_serial = normalize_string(failed_serial)
         query = f'INSERT INTO invalids VALUES ("{failed_serial}")'
         cursor.execute(query)
@@ -183,20 +233,24 @@ def create_tables():
     """
     connection = sqlite3.connect(config.DATABASE_FILE_PATH)
     cursor = connection.cursor()
-    cursor.execute('DROP TABLE IF EXISTS serials;')
-    cursor.execute("""CREATE TABLE IF NOT EXISTS serials (
+    cursor.execute("DROP TABLE IF EXISTS serials;")
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS serials (
             id INTEGER PRIMARY KEY,
             reference TEXT,
             description TEXT,
             start_serial TEXT,
             end_serial TEXT,
             date date
-        );""")
+        );"""
+    )
 
-    cursor.execute('DROP TABLE IF EXISTS invalids')
-    cursor.execute("""CREATE TABLE IF NOT EXISTS invalids (
+    cursor.execute("DROP TABLE IF EXISTS invalids")
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS invalids (
             failed_serial TEXT PRIMARY KEY
-        );""")
+        );"""
+    )
 
     connection.close()
 
@@ -211,7 +265,7 @@ def check_serial(serial):
         [string] -- check result
     """
     if len(serial) != 6:
-        return 'can not find your serial inside our db'
+        return "can not find your serial inside our db"
 
     connection = sqlite3.connect(config.DATABASE_FILE_PATH)
     cursor = connection.cursor()
@@ -220,17 +274,16 @@ def check_serial(serial):
     cursor.execute(query)
 
     if len(cursor.fetchall()) == 1:
-        return 'your serial is invalid'
+        return "your serial is invalid"
 
     query = f'SELECT * FROM serials WHERE start_serial < "{serial}" AND end_serial > "{serial}"'
     cursor.execute(query)
 
     if len(cursor.fetchall()) == 1:
-        return 'your serial is valid'
+        return "your serial is valid"
 
-    return 'can not find your serial inside our db'
+    return "can not find your serial inside our db"
 
 
-if __name__ == '__main__':
-    import_excel_to_db('../data.xlsx')
-    app.run('0.0.0.0', '5000', debug=True)
+if __name__ == "__main__":
+    app.run("0.0.0.0", "5000", debug=True)
